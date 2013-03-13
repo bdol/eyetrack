fc5ca34c9c701b133e7f3b007b191eb93a5b71f7
Brian Dolhansky <bdol@Brians-MacBook-Pro.local>
2013-03-13 19:37:51 -0400

    made a few changes in the matlab script that converts raw images

--- MyNiViewer/Debug/convert_image.m
+++ MyNiViewer/Debug/convert_image.m
@@ -1,20 +1,23 @@
 function I = convert_image(fName)
 
+xres = 1280;
+yres = 1024;
+
 fp=fopen(fName, 'rb');
-d = fread(fp, prod([640 480 3]));
+d = fread(fp, prod([xres yres 3]));
 red = d(1:3:end);
 green = d(2:3:end);
 blue = d(3:3:end);
-red = reshape(red,[640 480]);
-blue = reshape(blue,[640 480]);
-green = reshape(green,[640 480]);
+red = reshape(red,[xres yres]);
+blue = reshape(blue,[xres yres]);
+green = reshape(green,[xres yres]);
 
-im = zeros(640, 480, 3);
+im = zeros(xres, yres, 3);
 im(:,:,1) = red;
 im(:,:,2) = green;
 im(:,:,3) = blue;
 im = im./255;
-im_image = zeros(480, 640, 3);
+im_image = zeros(yres, xres, 3);
 im_image(:,:,1) = im(:,:,1)';
 im_image(:,:,2) = im(:,:,2)';
 im_image(:,:,3) = im(:,:,3)';
