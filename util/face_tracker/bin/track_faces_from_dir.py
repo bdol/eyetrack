f3515a307921f985f5bcbfdf5f216dd872d534a5
Brian Dolhansky <bdol@Brians-MacBook-Pro.local>
2013-04-04 09:45:25 -0400

    added script to automate single frame face extraction

---
+++ util/face_tracker/bin/track_faces_from_dir.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+import os, subprocess, sys
+
+if len(sys.argv)<4:
+    print "Usage: ./track_faces_from_dir <path_to_tracker_bin> <path_to_root_dir> <out_dir>"
+    sys.exit(1)
+
+exe = sys.argv[1]
+root = sys.argv[2]
+outDir = sys.argv[3]
+for path, subdirs, files in os.walk(root):
+    for name in files:
+        fName = os.path.join(path, name)
+
+        if name[0]!='I':
+            continue
+
+        print "Processing",fName
+        subprocess.call([exe, fName, outDir+"/face_"+name])
+
+
