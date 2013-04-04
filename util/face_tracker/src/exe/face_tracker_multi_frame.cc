72776be4e88316451db6a4ea7aa1505d26b57e1f
Brian Dolhansky <bdolmail@gmail.com>
2013-04-04 10:29:31 -0400

    added multi frame support to face tracker

---
+++ util/face_tracker/src/exe/face_tracker_multi_frame.cc
@@ -0,0 +1,168 @@
+///////////////////////////////////////////////////////////////////////////////
+// Copyright (C) 2010, Jason Mora Saragih, all rights reserved.
+// Modified by Brian Dolhansky, 2013. bdol@seas.upenn.edu
+// Pass a file with the frame list to this program.
+//
+//
+// This file is part of FaceTracker.
+//
+// Redistribution and use in source and binary forms, with or without 
+// modification, are permitted provided that the following conditions are met:
+//
+//     * The software is provided under the terms of this licence stricly for
+//       academic, non-commercial, not-for-profit purposes.
+//     * Redistributions of source code must retain the above copyright notice, 
+//       this list of conditions (licence) and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above copyright 
+//       notice, this list of conditions (licence) and the following disclaimer 
+//       in the documentation and/or other materials provided with the 
+//       distribution.
+//     * The name of the author may not be used to endorse or promote products 
+//       derived from this software without specific prior written permission.
+//     * As this software depends on other libraries, the user must adhere to 
+//       and keep in place any licencing terms of those libraries.
+//     * Any publications arising from the use of this software, including but
+//       not limited to academic journal and conference publications, technical
+//       reports and manuals, must cite the following work:
+//
+//       J. M. Saragih, S. Lucey, and J. F. Cohn. Face Alignment through 
+//       Subspace Constrained Mean-Shifts. International Conference of Computer 
+//       Vision (ICCV), September, 2009.
+//
+// THIS SOFTWARE IS PROVIDED BY THE AUTHOR "AS IS" AND ANY EXPRESS OR IMPLIED 
+// WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF 
+// MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO 
+// EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, 
+// INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES 
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+// ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF 
+// THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+///////////////////////////////////////////////////////////////////////////////
+#include <FaceTracker/Tracker.h>
+#include <opencv/highgui.h>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+//=============================================================================
+void Draw(cv::Mat &image,cv::Mat &shape,cv::Mat &con,cv::Mat &tri,cv::Mat &visi)
+{
+  int i,n = shape.rows/2; cv::Point p1,p2; cv::Scalar c;
+
+  //draw triangulation
+  c = CV_RGB(0,0,0);
+  for(i = 0; i < tri.rows; i++){
+    if(visi.at<int>(tri.at<int>(i,0),0) == 0 ||
+       visi.at<int>(tri.at<int>(i,1),0) == 0 ||
+       visi.at<int>(tri.at<int>(i,2),0) == 0)continue;
+    p1 = cv::Point(shape.at<double>(tri.at<int>(i,0),0),
+		   shape.at<double>(tri.at<int>(i,0)+n,0));
+    p2 = cv::Point(shape.at<double>(tri.at<int>(i,1),0),
+		   shape.at<double>(tri.at<int>(i,1)+n,0));
+    cv::line(image,p1,p2,c);
+    p1 = cv::Point(shape.at<double>(tri.at<int>(i,0),0),
+		   shape.at<double>(tri.at<int>(i,0)+n,0));
+    p2 = cv::Point(shape.at<double>(tri.at<int>(i,2),0),
+		   shape.at<double>(tri.at<int>(i,2)+n,0));
+    cv::line(image,p1,p2,c);
+    p1 = cv::Point(shape.at<double>(tri.at<int>(i,2),0),
+		   shape.at<double>(tri.at<int>(i,2)+n,0));
+    p2 = cv::Point(shape.at<double>(tri.at<int>(i,1),0),
+		   shape.at<double>(tri.at<int>(i,1)+n,0));
+    cv::line(image,p1,p2,c);
+  }
+  //draw connections
+  c = CV_RGB(0,0,255);
+  for(i = 0; i < con.cols; i++){
+    if(visi.at<int>(con.at<int>(0,i),0) == 0 ||
+       visi.at<int>(con.at<int>(1,i),0) == 0)continue;
+    p1 = cv::Point(shape.at<double>(con.at<int>(0,i),0),
+		   shape.at<double>(con.at<int>(0,i)+n,0));
+    p2 = cv::Point(shape.at<double>(con.at<int>(1,i),0),
+		   shape.at<double>(con.at<int>(1,i)+n,0));
+    cv::line(image,p1,p2,c,1);
+  }
+  //draw points
+  for(i = 0; i < n; i++){    
+    if(visi.at<int>(i,0) == 0)continue;
+    p1 = cv::Point(shape.at<double>(i,0),shape.at<double>(i+n,0));
+    c = CV_RGB(255,0,0); cv::circle(image,p1,2,c);
+  }return;
+}
+//=============================================================================
+int parse_cmd(int argc, const char** argv,
+	      char* ftFile,char* conFile,char* triFile,
+	      bool &fcheck,double &scale,int &fpd,
+          char* inFile, char* outDir)
+{
+    if (argc < 3) {
+        std::cout << "Usage: ./face_tracker_single_frame <infile> <outfile>" << std::endl;
+        return -1;
+    }
+    int i; fcheck = false; scale = 1; fpd = -1;
+    std::strcpy(ftFile,"../model/face2.tracker");
+    std::strcpy(conFile,"../model/face.con");
+    std::strcpy(triFile,"../model/face.tri");
+    std::strcpy(inFile, argv[1]);
+    std::strcpy(outDir, argv[2]);
+
+    return 0;
+}
+//=============================================================================
+int main(int argc, const char** argv)
+{
+    //parse command line arguments
+    char ftFile[256],conFile[256],triFile[256];
+    bool fcheck = false; double scale = 1; int fpd = -1; bool show = true;
+    char inFile[1024]; char outDir[1024];
+    if(parse_cmd(argc,argv,ftFile,conFile,triFile,fcheck,scale,fpd,inFile,outDir)<0)return 0;
+
+    //set other tracking parameters
+    std::vector<int> wSize1(1); wSize1[0] = 7;
+    std::vector<int> wSize2(3); wSize2[0] = 11; wSize2[1] = 9; wSize2[2] = 7;
+    int nIter = 5; double clamp=3,fTol=0.01; 
+    FACETRACKER::Tracker model(ftFile);
+    cv::Mat tri=FACETRACKER::IO::LoadTri(triFile);
+    cv::Mat con=FACETRACKER::IO::LoadCon(conFile);
+
+    cv::Mat frame,gray,im; double fps=0; char sss[256]; std::string text; 
+    int64 t1,t0 = cvGetTickCount(); int fnum=0;
+    bool failed = true;
+
+    std::ifstream frameFile(inFile);
+    std::string line;
+    int i=0;
+    while (std::getline(frameFile, line))
+    {
+        std::cout << line << std::endl; 
+        cv::Mat image = cv::imread(line, 1);
+        IplImage* I = cvCloneImage(&(IplImage)image);
+        frame = I;
+        if(scale == 1)im = frame; 
+        else cv::resize(frame,im,cv::Size(scale*frame.cols,scale*frame.rows));
+        cv::flip(im,im,1); cv::cvtColor(im,gray,CV_BGR2GRAY);
+
+        //track this image
+        std::vector<int> wSize; if(failed)wSize = wSize2; else wSize = wSize1; 
+        if(model.Track(gray,wSize,fpd,nIter,clamp,fTol,fcheck) == 0){
+          int idx = model._clm.GetViewIdx(); failed = false;
+          Draw(im,model._shape,con,tri,model._clm._visi[idx]); 
+        }else{
+          if(show){cv::Mat R(im,cvRect(0,0,150,50)); R = cv::Scalar(0,0,255);}
+          model.FrameReset(); failed = true;
+        }     
+
+        std::stringstream s;
+        s << outDir << i << ".png";
+        imwrite(s.str(), im);
+        
+        i++;
+    }
+
+
+   
+
+    return 0;
+}
+//=============================================================================
---
+++ util/face_tracker/src/exe/gen_frame_file.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+import os, sys
+
+if len(sys.argv)<3:
+    print "Usage: ./track_faces_from_dir <path_to_frame_folder> <out_file>"
+    sys.exit(1)
+
+root = sys.argv[1]
+outFile = sys.argv[2]
+f = open(outFile, "w")
+
+for path, subdirs, files in os.walk(root):
+    for name in files:
+        fName = os.path.join(path, name)
+        f.write(fName+"\n")
+
+f.close()
