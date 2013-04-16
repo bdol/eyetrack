b753d7f503776c233e8cef9e6ec32a486cb0a4a2
Brian Dolhansky <bdol@twn221-1.seas.upenn.edu>
2013-04-16 11:44:07 -0400

    started face rectification code

---
+++ util/face_rectification/src/face_rectification.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <opencv2/highgui/highgui.hpp>
+
+using namespace std;
+using namespace cv;
+
+int main(int argc, char** argv)
+{
+	Mat faceModel = Mat_<double>(50, 50);
+
+	return 0;
+}
