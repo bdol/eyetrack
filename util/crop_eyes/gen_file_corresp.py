c19b14780a0cec49aab6532864ff5350938a1a43
Brian Dolhansky <bdol@Brians-MacBook-Pro.local>
2013-04-18 20:02:04 -0400

    finished code to extract eye crops

--- util/crop_eyes/gen_file_corresp.py
+++ util/crop_eyes/gen_file_corresp.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-import os, sys
+import os, subprocess, sys
 
 if len(sys.argv)<2:
     print "Usage: ./gen_file_corresp <path_to_image_root>"
@@ -11,14 +11,15 @@ outFile = "im_files.txt"
 f = open(outFile, "w")
 
 # First get all the subject number base paths
-subjNumPaths = set([])
+subjNumPaths = []
 for path, subdirs, files in os.walk(root):
     for name in files:
         if name[0:2]=="IM":
             experimentName = os.path.basename(os.path.normpath(path))
             subjNumPath = path[0:-1]
             if subjNumPath not in subjNumPaths:
-                subjNumPaths.add(subjNumPath)
+                subjNumPaths.append(subjNumPath)
+
 
 # Now generate the file list
 for subjNumPath in subjNumPaths:
@@ -33,7 +34,7 @@ for subjNumPath in subjNumPaths:
 
     # Now let's generate the file list itself
     f.write("!\n")
-    f.write(canonicalPath+"\n")
+    f.write(canonicalPath.rstrip()+"\n")
     for expType in ["N", "P", "E"]:
         f.write("~"+expType+"\n")
         fList = os.listdir(subjNumPath+expType)
@@ -43,8 +44,91 @@ for subjNumPath in subjNumPaths:
 f.close()
 
 # Now using this file list, generate the correspondences
-imFile = open(outFile, "r")
-f = open("file_corresp.txt", "w")
+startPath = os.getcwd()
+binPath = "/Users/bdol/code/eyetrack/util/face_rectification/bin"
+imFile = open(startPath+"/"+outFile, "r")
+corrFile = open(startPath+"/fileCorresp.txt", "w")
+os.chdir(binPath)
 
+expNum = 0
+while 1:
+    expNameBase = os.path.basename(os.path.normpath(subjNumPaths[expNum]))
+    outPath =  "/Users/bdol/Desktop/corr_data/"+expNameBase
+    corrStrings = []
+    
+    # Skip this one, we already did it
+    if os.path.exists(outPath+"E/"):
+        if os.path.isfile(outPath+"E/H_27.txt"):
+            expNum += 1
+            for i in range(0, 86):
+                imFile.readline()
+            continue
 
-f.close()
+    imFile.readline()
+    imCanonical = imFile.readline()
+    corrStrings.append([imCanonical.rstrip(), outPath+"N/H_0.txt"])
+    imFile.readline()
+
+    # Generate correspondences for "N" experiment
+    f = open("frameListTemp.txt", "w")
+    f.write(imCanonical)
+    for i in range(1, 28):
+        imName = imFile.readline()
+        f.write(imName)
+        corrStrings.append([imName.rstrip(), outPath+"N/H_"+str(i)+".txt"])
+    f.close()
+
+    skip = False
+    if not os.path.exists(outPath+"N/"):
+        os.mkdir(outPath+"N/")
+    
+    subprocess.call(["./face_rectification", "frameListTemp.txt", outPath+"N/"])
+    # Now write these to the correspondence file
+    corrFile.write("!\n")
+    corrFile.write(corrStrings[0][0]+" "+corrStrings[0][1]+"\n")
+    corrFile.write("~N\n")
+    for i in range(1, 28):
+        corrFile.write(corrStrings[i][0]+" "+corrStrings[i][1]+"\n")
+
+    # Generate correspondences for "P" experiment
+    imFile.readline()
+    corrStrings = []
+    f = open("frameListTemp.txt", "w")
+    f.write(imCanonical)
+    for i in range(1, 28):
+        imName = imFile.readline()
+        f.write(imName)
+        corrStrings.append([imName.rstrip(), outPath+"P/H_"+str(i)+".txt"])
+    f.close()
+    if not os.path.exists(outPath+"P/"):
+        os.mkdir(outPath+"P/")
+    subprocess.call(["./face_rectification", "frameListTemp.txt", outPath+"P/"])
+    # Now write these to the correspondence file
+    corrFile.write("~P\n")
+    for i in range(0, 27):
+        corrFile.write(corrStrings[i][0]+" "+corrStrings[i][1]+"\n")
+
+    # Generate correspondences for "E" experiment
+    imFile.readline()
+    corrStrings = []
+    f = open("frameListTemp.txt", "w")
+    f.write(imCanonical)
+    for i in range(1, 28):
+        imName = imFile.readline()
+        f.write(imName)
+        corrStrings.append([imName.rstrip(), outPath+"E/H_"+str(i)+".txt"])
+    f.close()
+    if not os.path.exists(outPath+"E/"):
+        os.mkdir(outPath+"E/")
+
+    subprocess.call(["./face_rectification", "frameListTemp.txt", outPath+"E/"])
+    # Now write these to the correspondence file
+    corrFile.write("~E\n")
+    for i in range(0, 27):
+        corrFile.write(corrStrings[i][0]+" "+corrStrings[i][1]+"\n")
+
+    expNum += 1
+
+corrFile.close()
+imFile.close()
+os.chdir(startPath)
