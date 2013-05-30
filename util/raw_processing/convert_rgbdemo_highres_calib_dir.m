949fc41e861c90f66bf3236238fd815a27dd36ae
Brian Dolhansky <bdol@Brians-MacBook-Pro.local>
2013-05-30 17:39:53 -0400

    added code to get frames from .oni file

---
+++ util/raw_processing/convert_rgbdemo_highres_calib_dir.m
@@ -0,0 +1,23 @@
+raw_dir = '/Applications/RGBDemo/campostest2';
+out_dir = '~/Desktop/campostest2';
+D_rgb = rdir([raw_dir '/*/*/*/color.png']);
+D_depth = rdir([raw_dir '/*/*/*/depth.raw']);
+
+fprintf('Converting images...\n');
+
+c = 0;
+for i=1:numel(D_rgb)
+    I_rgb = imread(D_rgb(i).name);
+    I_rgb = I_rgb(1:960, :, :);
+    I_depth = convert_rgbdemo_raw(D_depth(i).name);
+    
+    rgb_fname = [out_dir '/tv_rgb_' num2str(c) '.png'];
+    depth_fname = [out_dir '/tv_depth_' num2str(c) '.raw'];
+
+    imwrite(I_rgb, rgb_fname);
+    copyfile(D_depth(i).name, depth_fname);
+    
+    c = c+1;
+end
+
+fprintf('Done!\n');
