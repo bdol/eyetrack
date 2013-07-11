3cf7ab37c9f65430c010d9458b989484172a92b5
Varsha Shankar <fawkes.gryffindor@gmail.com>
2013-07-11 14:44:21 -0400

    field of view display for head pose with two kinects

---
+++ code/table_view/old/matlab/vector_plane_intersection.m
@@ -0,0 +1,14 @@
+function Pintersect = vector_plane_intersection(P, o, d)
+% P = [X ;Y; Z] points on the plane >=3
+% o = [o1 o2 o3] origin of vector
+% d = [d1 d2 d3] direction of vector
+
+% pick three points on the plane
+rand_idx = randi([1, size(P,2)], 3, 1);
+poin1 = P(:,rand_idx(1));
+point2 = P(:,rand_idx(2));
+point3 = P(:,rand_idx(3));
+normal = cross(poin1-point2, poin1-point3);
+t = - dot(normal, [o(:) - point2(:)]) / dot(normal, d);
+
+Pintersect = o(:) + t.*d(:);
